766b83d9230a43f48e786e3ac86299ce34815da1
Miyuki-Satomura <miyuki.wookun@gmail.com>
2017-12-23 15:22:33 +0000

    top page

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -51,7 +51,7 @@
 @section('content')
     <div class="center jumbotron">
         <div class="text-center">
-            <h1>Welcome to the Microposts</h1>
+            <h1>Welcome to the Tasklist</h1>
         </div>
     </div>
 @endsection
