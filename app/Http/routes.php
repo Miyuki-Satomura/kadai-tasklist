5eac0fcb60b252589b23450d419e294663d29ffc
Miyuki-Satomura <miyuki.wookun@gmail.com>
2017-12-23 17:56:39 +0000

    user registration

--- app/Http/routes.php
+++ app/Http/routes.php
@@ -18,3 +18,7 @@ Route::resource('tasks', 'TasksController');
 Route::get('/', function () {
     return view('welcome');
 });
+
+// ユーザ登録
+Route::get('signup', 'Auth\AuthController@getRegister')->name('signup.get');
+Route::post('signup', 'Auth\AuthController@postRegister')->name('signup.post');
