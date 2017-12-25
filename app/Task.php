2c6c946699066ca0ce9841ac566a8c901b04a9d8
Miyuki-Satomura <miyuki.wookun@gmail.com>
2017-12-25 10:36:22 +0000

    task.php fillable

--- app/Task.php
+++ app/Task.php
@@ -6,5 +6,11 @@ use Illuminate\Database\Eloquent\Model;
 
 class Task extends Model
 {
-    //
+    // 追加
+    protected $fillable = ['content', 'user_id'];
+
+    public function user()
+    {
+        return $this->belongsTo(User::class);
+    }
 }
